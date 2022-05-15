c02f093029d530f9a36c5bfedda1e5dd232ea143
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-05-15 23:09:29 +0200

    initial

---
+++ Data/FootballTeam.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MudBlazorDemoServer.Data
+{
+    public class FootballTeam
+    {
+        public FootballTeam()
+        {
+
+        }
+        public FootballTeam(string name, string stadium, DateTime? foundationYear, string city, int rating, int supporterCount, string colors)
+        {
+            Name = name;
+            Stadium = stadium;
+            FoundationYear = foundationYear;
+            City = city;
+            Rating = rating;
+            SupporterCount = supporterCount;
+            Colors = colors;
+        }
+        public string Name { get; set; }
+        public string Stadium { get; set; }
+        public DateTime? FoundationYear { get; set; }
+        public string City { get; set; }
+
+        [Required]
+        [Range(1, 10, ErrorMessage = "Value should be between {1} and {2}")]
+        public int Rating { get; set; }
+        public int SupporterCount { get; set; }
+        public string Colors { get; set; }
+    }
+}
---
+++ Data/FootballTeamsService.cs
@@ -0,0 +1,29 @@
+﻿namespace MudBlazorDemoServer.Data
+{
+    public class FootballTeamsService
+    {
+        public Task<List<FootballTeam>> Get()
+        {
+            return Task.FromResult(footballTeams);
+        }
+
+        public void AddFootballTeam(FootballTeam model)
+        {
+            footballTeams.Add(model);
+        }
+
+        public bool Delete(string name)
+        {
+            var item = footballTeams.FirstOrDefault(x => x.Name == name);
+            var result = footballTeams.Remove(item);
+            return result;
+        }
+
+        private static readonly List<FootballTeam> footballTeams = new()
+        {
+            new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000")
+        };
+    }
+}
