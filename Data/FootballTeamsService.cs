61a835261017ec1b1dfac1907aff25b7542fafd7
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-05-24 17:02:49 +0200

    server side page and search implemented

--- Data/FootballTeamsService.cs
+++ Data/FootballTeamsService.cs
@@ -19,9 +19,99 @@
             return result;
         }
 
+        public Tuple<IEnumerable<FootballTeam>, int> GetPagedData(int page, int pageSize)
+        {
+
+            var data = footballTeams.Skip(page * pageSize).Take(pageSize).ToArray();
+            var count = footballTeams.Count;
+            return new Tuple<IEnumerable<FootballTeam>, int>(data, count);
+
+        }
+
+        public Tuple<IEnumerable<FootballTeam>, int> GetSearchedData(string searchText, int page, int pageSize)
+        {
+
+            var filtered = footballTeams.Where(element =>
+            {
+                if (string.IsNullOrWhiteSpace(searchText))
+                    return true;
+                if (element.Name.Contains(searchText, StringComparison.OrdinalIgnoreCase))
+                    return true;
+                if (element.Stadium.Contains(searchText, StringComparison.OrdinalIgnoreCase))
+                    return true;
+                if (element.City.Contains(searchText, StringComparison.OrdinalIgnoreCase))
+                    return true;
+                return false;
+            }).ToArray();
+
+            var data = filtered.Skip(page * pageSize).Take(pageSize).ToArray();
+            var count = filtered.Length;
+            return new Tuple<IEnumerable<FootballTeam>, int>(data, count);
+
+
+        }
+
+
         private static readonly List<FootballTeam> footballTeams = new()
         {
             new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+            new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+            new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+            new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
+            new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
+            new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000"),
+              new FootballTeam("Fenerbahçe", "Şükrü Saraçoğlu", new DateTime(1907, 1, 1), "İstanbul", 10, 20_000_000, "#FFEB3B,#0277BD"),
             new FootballTeam("Galatasaray", "TTArena", new DateTime(1905, 1, 1), "İstanbul", 5, 22_000_000, "#FFEB3B,#FF5722"),
             new FootballTeam("Beşiktaş", "Vodafone Arena", new DateTime(1903, 1, 1), "İstanbul", 4, 15_000_000, "#FFFFFF,#000000")
         };
---
+++ Pages/FootballTeams.razor.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+using MudBlazorDemoServer.Data;
+
+namespace MudBlazorDemoServer.Pages
+{
+
+    public partial class FootballTeams : ComponentBase
+    {
+
+        [Inject] protected FootballTeamsService footballTeamsService { get; set; }
+        [Inject] protected ISnackbar Snackbar { get; set; }
+        private MudTable<FootballTeam> table;
+
+        FootballTeam model = new FootballTeam();
+        public List<FootballTeam> footballTeams;
+        private IEnumerable<FootballTeam> searchedData;
+        double[] chartData;
+        string[] teams;
+        private string colorValue1;
+        private string colorValue2;
+        private int totalItems;
+        private string searchString;
+
+        protected async Task<TableData<FootballTeam>> ServerReload(TableState state)
+        {
+            var data = footballTeamsService.GetSearchedData(searchString, state.Page, state.PageSize);
+            searchedData = data.Item1;
+            totalItems = data.Item2;
+            DrawChart();
+            return new TableData<FootballTeam>() { TotalItems = totalItems, Items = searchedData };
+        }
+
+        private void OnValidSubmit(EditContext context)
+        {
+            model.Colors = string.Join(",", new string[] { colorValue1, colorValue2 });
+            footballTeamsService.AddFootballTeam(model);
+            Snackbar.Add("The team added successfully!", Severity.Success);
+            CleanUp();
+            DrawChart();
+        }
+
+        private void Delete(string name)
+        {
+            bool result;
+            result = footballTeamsService.Delete(name);
+
+            if (result)
+            {
+                Snackbar.Add("The team removed successfully!", Severity.Success);
+                DrawChart();
+            }
+            else
+            {
+                Snackbar.Add("An error occured!", Severity.Error);
+            }
+        }
+
+        private void CleanUp()
+        {
+            model = new FootballTeam();
+            colorValue1 = "";
+            colorValue2 = "";
+        }
+
+        public void DrawChart()
+        {
+            teams = searchedData.Select(s => s.Name).ToArray();
+            chartData = searchedData.Select(x => Convert.ToDouble(x.SupporterCount)).ToArray();
+            StateHasChanged();
+        }
+
+        private void OnSearch(string text)
+        {
+            searchString = text;
+            table.ReloadServerData();
+        }
+    }
+}
